97be3dfff43279b1c6378da28a5ee76205c086ee
Yu-Wei Wu <wusyong9104@gmail.com>
2019-11-17 19:18:08 +0800

    Add signature and rest of fking owl

--- src/hash.rs
+++ src/hash.rs
@@ -2,8 +2,8 @@ use sha2::{Digest, Sha256};
 
 use crate::N;
 
-const PADDING_F: u8 = 0;
-const PADDING_PRF: u8 = 3;
+const PADDING_F: usize = 0;
+const PADDING_PRF: usize = 3;
 
 fn core_hash(input: &[u8]) -> [u8; N] {
     let mut hasher = Sha256::new();
@@ -15,9 +15,7 @@ fn core_hash(input: &[u8]) -> [u8; N] {
 pub fn prf(key: &[u8; N], input: &[u8; 32]) -> [u8; N] {
     let mut buf = [0u8; 2 * N + 32];
 
-    for i in 0..N {
-        buf[i] = PADDING_PRF;
-    }
+    byte_array(&mut buf[0..N], PADDING_PRF);
 
     for i in 0..N {
         buf[N + i] = key[i];
@@ -34,9 +32,7 @@ pub fn prf(key: &[u8; N], input: &[u8; 32]) -> [u8; N] {
 pub fn hash_f(key: &[u8; N], input: &[u8; N]) -> [u8; N] {
     let mut buf = [0u8; 3 * N];
 
-    for i in 0..N {
-        buf[i] = PADDING_F;
-    }
+    byte_array(&mut buf[0..N], PADDING_F);
 
     for i in 0..N {
         buf[N + i] = key[i];
@@ -47,4 +43,11 @@ pub fn hash_f(key: &[u8; N], input: &[u8; N]) -> [u8; N] {
     }
 
     core_hash(&buf)
+}
+
+pub fn byte_array(output: &mut [u8], mut input: usize) {
+    for i in (0..output.len()).rev() {
+        output[i] = (input & 0xff) as u8;
+        input = input >> 8;
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,13 @@
 use rand;
 
 mod hash;
-use hash::{hash_f, prf};
+use hash::{byte_array, hash_f, prf};
 
 /// The message length as well as the length of a private key, public key, or signature element in bytes.
 const N: usize = 32;
 /// The Winternitz parameter; it is a member of the set {4, 16}.
 const W: usize = 16;
+const LOG_W: usize = 4;
 /// L1 = ceil(8N / lg(W))
 const L1: usize = 64;
 /// L2 = floor(lg(L1 * (W - 1)) / lg(W)) + 1
@@ -65,12 +66,53 @@ pub struct SecKey {
     address: Adrs,
 }
 
+/// WOTS+ signature
+#[derive(Clone, Copy)]
+pub struct Signature {
+    inner: [u8; N * LEN],
+    pub_seed: Seed,
+    address: Adrs,
+}
+
 impl SecKey {
     pub fn new() -> Self {
         Self {
             seed: Seed::new(),
             address: Adrs::new(),
         }
+    }
+
+    pub fn set_seed(&mut self, seed: Seed) {
+        self.seed = seed;
+    }
+
+    pub fn set_address(&mut self, address: Adrs) {
+        self.address = address;
+    }
+
+    /// Takes a n-byte message and the 32-byte seed for the private key to compute and return a signature.
+    pub fn sign(&self, pub_seed: &Seed, msg: &[u8; N]) -> Signature {
+        let lengths = concatenation(msg);
+        let mut address = self.address;
+        let mut sig = Signature {
+            inner: [0; N * LEN],
+            pub_seed: *pub_seed,
+            address: self.address,
+        };
+
+        for (i, n) in lengths.iter().enumerate() {
+            address.set_chain(i as u32);
+            chain(
+                &mut sig.inner[(i * N)..(i * N + N)],
+                &prf(&self.seed.0, &address.0), // i-th secret key
+                0,
+                *n as usize,
+                pub_seed,
+                &mut address,
+            );
+        }
+
+        sig
     }
 }
 
@@ -86,20 +128,19 @@ impl PubKey {
     /// WOTS public key generation. Takes a 32 byte seed for the private key, expands it to
     /// a full WOTS private key and computes the corresponding public key.
     /// It requires the private key and a pub_seed (used to generate bitmasks and hash keys).
-    pub fn from_sec_key(sec_key: &SecKey, pub_seed: &Seed) -> Self {
-        let mut pub_key = Self {
+    pub fn from_seckey(seckey: &SecKey, pub_seed: &Seed) -> Self {
+        let mut pubkey = Self {
             inner: [0; N * LEN],
             pub_seed: *pub_seed,
-            address: sec_key.address,
+            address: seckey.address,
         };
 
-        let mut address = sec_key.address;
+        let mut address = seckey.address;
         for i in 0..LEN {
             address.set_chain(i as u32);
-            let sec_key = prf(&sec_key.seed.0, &address.0);
             chain(
-                &mut pub_key.inner[(i * N)..(i * N + N)],
-                &sec_key,
+                &mut pubkey.inner[(i * N)..(i * N + N)],
+                &prf(&seckey.seed.0, &address.0), // i-th secret key
                 0,
                 W - 1,
                 pub_seed,
@@ -107,7 +148,30 @@ impl PubKey {
             );
         }
 
-        pub_key
+        pubkey
+    }
+
+    pub fn from_signature(sig: &Signature, msg: &[u8; N]) -> Self {
+        let lengths = concatenation(msg);
+        let mut address = sig.address;
+        let mut pubkey = Self {
+            inner: [0; N * LEN],
+            pub_seed: sig.pub_seed,
+            address: sig.address,
+        };
+
+        for (i, n) in lengths.iter().enumerate() {
+            chain(
+                &mut pubkey.inner[(i * N)..(i * N + N)],
+                &sig.inner[(i * N)..(i * N + N)],
+                *n as usize,
+                W - 1 - *n as usize,
+                &sig.pub_seed,
+                &mut address,
+            );
+        }
+
+        pubkey
     }
 }
 
@@ -117,7 +181,7 @@ impl PubKey {
 /// address has to contain the address of the chain.
 fn chain(
     output: &mut [u8],
-    input: &[u8; N],
+    input: &[u8],
     start: usize,
     steps: usize,
     pub_seed: &Seed,
@@ -135,5 +199,45 @@ fn chain(
             }
             output.copy_from_slice(&hash_f(&key, &bitmask));
         }
+    }
+}
+
+/// Concatenation of the base-`W` representations of the message and its checksum.
+fn concatenation(msg: &[u8; N]) -> [u8; LEN] {
+    let mut output = [0u8; LEN];
+
+    // Compute base-`W` representations of the message
+    base_w(&mut output[0..L1], msg);
+
+    // Computes checksum
+    let mut csum = output[0..L1]
+        .iter()
+        .fold(0, |acc, &x| acc + W - 1 - x as usize);
+
+    // Convert checksum to base_w.
+    // Make sure expected empty zero bits are the least significant bits.
+    csum = csum << (8 - ((L2 * LOG_W) % 8));
+    let mut csum_bytes = [0u8; ((L2 * LOG_W) + 7) / 8];
+    byte_array(&mut csum_bytes, csum);
+    base_w(&mut output[L1..LEN], &csum_bytes);
+
+    output
+}
+
+/// base_w algorithm as described in draft.
+/// Interprets an array of bytes as integers in base w.
+/// This only works when log_w is a divisor of 8.
+fn base_w(output: &mut [u8], input: &[u8]) {
+    let mut i = 0;
+    let mut bits = 0;
+    let mut total: u8 = 0;
+    for out in output.iter_mut() {
+        if bits == 0 {
+            total = input[i];
+            i += 1;
+            bits += 8;
+        }
+        bits -= LOG_W;
+        *out = (total >> bits) & (W - 1) as u8;
     }
 }
